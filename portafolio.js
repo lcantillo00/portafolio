ad8c450e27135e5192540c1f0fca836bf889459d
FVI <test@examle.com>
2017-09-20 17:21:36 -0400

    last version

--- portafolio.js
+++ portafolio.js
@@ -1,17 +1,15 @@
-(function(document) {
-  var _bars = [].slice.call(document.querySelectorAll('.bar-inner'));
-  _bars.map(function(bar, index) {
-    setTimeout(function() {
-    	bar.style.width = bar.dataset.percent;
-    }, index * 1000);
-    
-})(document)
+$(document).ready(function(){
+    $('.skilllink').click(function(){
+        $('.js').animate({width:'90%'},2000);
+        $('.py').animate({width:'95%'},2000);
+        $('.html').animate({width:'95%'},2000);
+        $('.sql').animate({width:'75%'},2000);
+        $('.mongo').animate({width:'75%'},2000);
+        $('.git').animate({width:'100%'},2000);
+        $('.node').animate({width:'70%'},2000);
+        $('.flex').animate({width:'95%'},2000);
+        $('.react').animate({width:'75%'},2000);
+        $('.jquery').animate({width:'95%'},2000);
+    })
 
-// function scaleUp(obj, size)
-// {
-//     obj.style.size=
-// }
-
-// $(document).ready(function(){
-//
-// });
+        	});
